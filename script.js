1b304763f33d00593d9b9b353902ffe67e4055c0
Nadi Duno <nadiduno@gmail.com>
2022-10-17 21:43:29 -0300

    Usando JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+var age = prompt("Qual é sua idade?: ");
+var ageInMonth=age*12;
+var ageInSeconds=age*365*24*60*60;
+document.write("Você têm " + age + " anos");
+document.write("\n\nque equivale a " + ageInMonth + " meses de idade");
+document.write("\n\ne a " + ageInSeconds + " segundos de vida");
